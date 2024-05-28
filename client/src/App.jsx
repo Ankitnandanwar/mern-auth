6140cbbca64f2171b5e0bd078871ff0c7a252fb0
Ankitnandanwar <ankitnandanwar3@gmail.com>
2024-05-28 12:18:39 +0530

    install react js and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-500 font-semibold text-[30px]'>App</h1>
+    </div>
+  )
+}
+
+export default App
