d1af00970b4896096e011f83fb88ad49a8017041
Ankitnandanwar <ankitnandanwar3@gmail.com>
2024-05-28 23:33:27 +0530

    added middleware & handled error

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ const app = express();
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODBURL)
-.then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.log(err)
-})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((err) => {
+        console.log(err)
+    })
 
 app.listen(8500, () => {
     console.log('Server is running on port 8500');
@@ -23,3 +23,14 @@ app.listen(8500, () => {
 
 app.use("/user", userRoutes)
 app.use("/auth", authRoutes)
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Something went wrong";
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    });
+})
