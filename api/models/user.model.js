79ec557e7ebf70f484477a0399f46b385682e413
Ankitnandanwar <ankitnandanwar3@gmail.com>
2024-08-22 21:18:29 +0530

    create and add OAuth component (firebase SignIn & SignUp with google account)

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,7 +16,12 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    profilePicture:{
+        type:String,
+        default: "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?t=st=1724339703~exp=1724343303~hmac=57416d9b469e467d64864860f886bfdde1439e97000977cc737627fe574585a6&w=740"
+    },
+
 }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema)
--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { signinStart, signinSuccess, signinFailed } from '../Redux/user/userSlice'
 import { useDispatch, useSelector } from 'react-redux'
+import OAuth from '../components/OAuth'
 
 const SignIn = () => {
 
@@ -48,6 +49,7 @@ const SignIn = () => {
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'Loading...' : 'Sign In'}
         </button>
+        <OAuth/>
       </form>
       <div className='flex mt-5 gap-2'>
         <p>Dont have an account ?</p>
